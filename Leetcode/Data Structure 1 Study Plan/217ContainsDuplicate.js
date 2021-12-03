a145d424e535a79bf7514b6da133b2730a78c7c6
Alice Won <alice.w@accretivetg.com>
2021-12-03 11:56:46 -0800

    adding links to problems

--- Leetcode/Data Structure 1 Study Plan/217ContainsDuplicate.js
+++ Leetcode/Data Structure 1 Study Plan/217ContainsDuplicate.js
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/contains-duplicate/
 
+// Problem:
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
 // SOLUTION 1: using Map();
--- Leetcode/Data Structure 1 Study Plan/53MaximumSubarray.js
+++ Leetcode/Data Structure 1 Study Plan/53MaximumSubarray.js
@@ -1,3 +1,9 @@
+// https://leetcode.com/problems/maximum-subarray/
+
+// Problem:
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+// A subarray is a contiguous part of an array.
+
 var maxSubArray = function(nums) {
   let maxSum = nums[0];
   let current = maxSum;
